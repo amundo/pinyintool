20d22c5038ee83bf4f23f797fb3f3de4ef08710f
Patrick Hall <pat@amundo.com>
2012-09-27 15:54:46 -0700

    add ü support, now with non-combining characters

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ pinyintool
 ==========
 
 Simple web-based tool for transliterating Pinyin.
+
+Demo:
+
+http://lotsofwords.org/languages/chinese/pinyintool
--- zh.js
+++ zh.js
@@ -1,12 +1,37 @@
 app = {};
 
+app.rules = [
+  [ "a1", "\u0101" ], 
+  [ "a2", "\u00e1" ], 
+  [ "a3", "\u01ce" ], 
+  [ "a4", "\u00e0" ], 
+  [ "e1", "\u0113" ], 
+  [ "e2", "\u00e9" ], 
+  [ "e3", "\u011b" ], 
+  [ "e4", "\u00e8" ], 
+  [ "i1", "\u012b" ], 
+  [ "i2", "\u00ed" ], 
+  [ "i3", "\u01d0" ], 
+  [ "i4", "\u00ec" ], 
+  [ "o1", "\u014d" ], 
+  [ "o2", "\u00f3" ], 
+  [ "o3", "\u01d2" ], 
+  [ "o4", "\u00f2" ], 
+  [ "u1", "\u016b" ], 
+  [ "u2", "\u00fa" ], 
+  [ "u3", "\u01d4" ], 
+  [ "u4", "\u00f9" ], 
+  [ "v1", "\u01d6" ], 
+  [ "v2", "\u01d8" ], 
+  [ "v3", "\u01da" ], 
+  [ "v4", "\u01dc" ]
+]
+
 $(document).ready(function(){
 
     function transliterate(text){
 
-      var rules = [ [ "1", "\u0304" ], [ "2", "\u0301" ], [ "3", "\u030C" ], [ "4", "\u0300" ], ["v", "ü"] ];
-
-      $.each(rules, function(i, rule){
+      $.each(app.rules, function(i, rule){
         var beforeRE = new RegExp(rule[0], 'g'),
             after = rule[1];
 
